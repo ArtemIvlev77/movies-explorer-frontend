f1a90cdb623d1daf0fe99eadc4fe3741ed6c8b42
Artem <artemivlev@yandex.ru>
2021-06-19 00:50:55 +0300

    feat: вёрстка блока main
    Добавлены компоненты:
    -AboutMe
    -Techs
    -Portfolio

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import React from "react";
+import "./AboutMe.css";
+import photo from "../../../images/photo.svg";
 
 function AboutMe() {
   return (
-<p>AboutME</p>
-  )
-};
+    <div className="aboutMe">
+      <h2 className="aboutMe__title" id="aboutMe">
+        Студент
+      </h2>
+      <div className="aboutMe__content">
+        <div className="aboutMe__text">
+          <h3 className="aboutMe__name">Артём</h3>
+          <span className="aboutMe__subtitle">
+            Фронтенд-разработчик, 31 год
+          </span>
+          <p className="aboutMe__content_about">
+            Я родился в Саратове и с 2015 года живу в Москве. У меня есть жена и
+            сын. Я люблю слушать музыку, а ещё увлекаюсь ездой на велосипеде.
+            Недавно, c 2020 года начал кодить. Так же в 2020 году приступил к
+            прохождению курса веб-разработчика в Яндекс.Практикуме. Ближе к
+            окончанию курса стал брать фриланс-заказы.
+          </p>
+          <ul className="aboutMe__content_social">
+            <li>
+              <a
+                className="aboutMe__content_link"
+                href="https://www.facebook.com/artem.ivlev.1/"
+                target="_blank" rel="noopener noreferrer"
+              >
+                Facebook
+              </a>
+            </li>
+            <li>
+              <a
+                className="aboutMe__content_link"
+                href="https://github.com/ArtemIvlev77"
+                target="_blank" rel="noopener noreferrer"
+              >
+                Github
+              </a>
+            </li>
+            <li>
+              <a
+                className="aboutMe__content_link"
+                href="https://www.linkedin.com/in/artem-ivlev-216ab4210/"
+                target="_blank" rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+        </div>
+        <img
+          className="aboutMe__content_photo"
+          src={photo}
+          alt="Фото студента"
+        />
+      </div>
+    </div>
+  );
+}
 
 export default AboutMe;
--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import "./Portfolio.css";
+import arrow from "../../../images/arrow.svg";
 
 function Portfolio() {
-  return <p>Portofolio</p>;
+  return (
+    <div className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__list">
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="/#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="portfolio__item_title">Статичный сайт</p>
+            <img className="portfolio__arrow" src={arrow} alt="arrow" />
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="/#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="portfolio__item_title">Адаптивный сайт</p>
+            <img className="portfolio__arrow" src={arrow} alt="arrow" />
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="/#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="portfolio__item_title">Одностраничное приложение</p>
+            <img className="portfolio__arrow" src={arrow} alt="arrow" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Portfolio;
