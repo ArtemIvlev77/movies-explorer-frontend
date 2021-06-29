d2d68296ef47ca3909adf3d492bcaf0583f4df73
Artem <artemivlev@yandex.ru>
2021-06-29 12:08:55 +0300

    feat: исправлены состояния кнопки карточки

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import "./MoviesCard.css";
 import card from "../../../images/card.png";
@@ -6,8 +6,12 @@ import card from "../../../images/card.png";
 function MoviesCard() {
   const { pathname } = useLocation();
 
-  const cardIcon =
-    pathname === "/movies" ? "card__favorite-btn" : "card__close-btn";
+  const [favoriteMovie, setFavoriteMovie] = useState(false);
+  const likeIcon = favoriteMovie
+    ? "card__favorite-btn_active"
+    : "card__favorite-btn";
+
+  const cardIcon = pathname === "/movies" ? likeIcon : "card__close-btn";
 
   return (
     <li className="card">
@@ -16,7 +20,11 @@ function MoviesCard() {
           <p className="card__name">33 слова о дизайне</p>
           <p className="card__duration">1ч 47мин</p>
         </div>
-        <button className={cardIcon} type="button"></button>
+        <button
+          className={(cardIcon)}
+          type="button"
+          onClick={setFavoriteMovie}
+        ></button>
       </div>
       <img className="card__image" src={card} alt="карточка фильма" />
     </li>
