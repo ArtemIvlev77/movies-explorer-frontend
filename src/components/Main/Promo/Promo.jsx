370a86fa001696412386297b8b64dc95f2da8a80
Artem <artemivlev@yandex.ru>
2021-08-09 15:44:41 +0300

    yarn add

--- src/components/Main/Promo/Promo.jsx
+++ src/components/Main/Promo/Promo.jsx
@@ -6,14 +6,16 @@ function Promo() {
   return (
     <div className="promo">
       <div className="promo__wrapper">
-      <div className="promo__text-content">
-        <h1 className="promo__title">
-          Учебный проект студента факультета Веб-разработки.
-        </h1>
-        <h3 className="promo__subtitle">
-          Листайте ниже, чтобы узнать больше про этот проект и его создателя.
-        </h3>
-        <button className="promo__learn-more">Узнать больше</button>
+        <div className="promo__text-content">
+          <h1 className="promo__title">
+            Учебный проект студента факультета Веб-разработки.
+          </h1>
+          <h3 className="promo__subtitle">
+            Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+          </h3>
+          <a href="#about" className="learn-more_link">
+            <button className="promo__learn-more">Узнать больше</button>
+          </a>
         </div>
         <img src={www} alt="www" className="promo__image" />
       </div>
